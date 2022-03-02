6ba360327ec9501b9bf998193bb15cf85b8660aa
Edgar Fernandez <tempurakohi@gmail.com>
2022-03-02 01:52:19 -0800

    attempted to put the upload file on profile picture

--- client/src/components/Profile/Upload.js
+++ client/src/components/Profile/Upload.js
@@ -1,21 +1,44 @@
-import React from 'react';
 import "../../assets/style.css"
+import React, {Component} from "react"
 import {
-    Row,
-    Container,
-    Card
-  } from 'react-bootstrap'
+  Row,
+  Container,
+  Card
+} from 'react-bootstrap'
 
-const Upload = () => {
+
+class Upload extends Component{
+  state ={
+    selectedFile:null
+  }
+
+  fileSelectedHandler = event => {
+    this.setState({
+      selectedFile: event.target
+    })
+  }
+
+  FileUploadHandler =()=>{
+    const fd = new FormData
+    fd.append("image",this.state.selectedFile, this.state.selectedFile.name)
+  }
+
+  render(){
   return (
-    <Container>
+    <><Container>
       <Row>
         <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src="../logo192.png" style={{ height: '18rem' }}/>
+          <Card.Img variant="top" src="../logo192.png" style={{ height: '18rem' }} />
         </Card>
       </Row>
-</Container>
+    </Container>
+    <div className="App">
+      <input style={{display: "none"}}type="file" onChange={this.fileSelectedHandler} ref={fileInput => this.fileInput = fileInput}/>
+      <button onClick={() => this.fileInput.click(),this.fileSelectedHandler}>Upload</button>
+      <button onClick={this.FileUploadHandler}>Save</button>
+    </div></>
   );
-};
+}
+}
 
 export default Upload;
--- client/src/pages/products.js
+++ client/src/pages/products.js
@@ -1,43 +1,28 @@
 import React from 'react';
-
+import "../assets/product.css"
 
 const Product =() => {
 return(
-    <div>
-    <h1>THIS IS OUR PRODUCT PAGE</h1>
-  </div>
-)
-}
-var NewComponent = React.createClass({
-  render: function() {
-    return (
-      <div>
-        <meta charSet="UTF-8" />   
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="stylesheet" href="style.css" />
-        <title>Page Title</title>
-        <section className="container">
-          <div className="card">
-            <div className="card-image website-1" />
+<section className="containerCont">
+          <div className="cardCont">
+            <div className="cardCont-image website-1" />
             <h2>Simple</h2>
             <p>This is a simple page for beginners.</p>
             <a href>Purchase</a>
           </div>
-          <div className="card">
-            <div className="card-image website-2" />
+          <div className="cardCont">
+            <div className="cardCont-image website-2" />
             <h2>Medium</h2>
             <p>This is a medium tier website. </p>
             <a href>Purchase</a>
           </div>
-          <div className="card">
-            <div className="card-image wesbite-3" />
+          <div className="cardCont">
+            <div className="cardCont-image wesbite-3" />
             <h2>Advanved</h2>
             <p>This is for someone who wants to take control of their entire wesbite.</p>
             <a href>Purchase</a>
           </div>
         </section>
-      </div>
-    );
-  }
-});
+)
+}
 export default Product
