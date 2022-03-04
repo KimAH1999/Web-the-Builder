950479e7b0889781892c6be568181d8b677e9366
Kimberly Aguilar Hermoso <kimaguilar2017@gmail.com>
2022-03-04 15:48:28 -0800

    Update index.js

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Auth from '../../utils/auth';
-import { Link } from 'react-router-dom';
 import {Navbar,Container,Nav} from 'react-bootstrap';
 import logo from "../../assets/hammer.gif"
 
