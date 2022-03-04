8bf86ae54aec044449edf0a10c7c04daa8c06953
Edgar Fernandez <tempurakohi@gmail.com>
2022-03-03 17:45:31 -0800

    saving changes, trying to get styling back

---
+++ client/src/components/Profile/Upload.js
@@ -0,0 +1,36 @@
+import React from "react"
+import {Row,Container,Card} from 'react-bootstrap'
+
+
+const Upload = () => {
+  const uploadImage = React.useRef(null)
+  const imageUploader = React.useRef(null)
+
+  const handleImageUpload = e =>{
+    const [file] = e.target.files
+    if (file) {
+      const reader = new FileReader()
+      const {current} = uploadImage
+      current.file= file
+      reader.onload=(e)=>{
+        current.src= e.target.result
+      }
+      reader.readAsDataURL(file)
+    }
+  }
+
+  return (
+    <Container>
+      <Row>
+        <Card style={{ width: '18rem' }} onClick={(()=> imageUploader.current.click())}>
+          <img ref={uploadImage} variant="top" src="../logo192.png" alt="profile picture" style={{ height: '18rem' }} />
+          <input ref={imageUploader} type="file" accept="image" multiple={false} onChange={handleImageUpload} style={{display: "none"}}/>
+        </Card>
+      </Row>
+    </Container>
+
+  );
+}
+
+
+export default Upload;
