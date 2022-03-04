2cdd3a94bb08df9b9b5d03486d487109838aace5
Edgar Fernandez <tempurakohi@gmail.com>
2022-03-03 23:28:36 -0800

    editing the nav bar

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -7,12 +7,23 @@ function Nav() {
     if (Auth.loggedIn()) {
       return (
         <ul className="flex-row">
+
+          <li className="mx-1">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="mx-1">
+            <Link to="/products">Products</Link>
+          </li>
+          <li className="mx-1">
+            <Link to="/Contact">Contact</Link>
+          </li>
           <li className="mx-1">
             <Link to="/orderHistory">Order History</Link>
           </li>
           <li className="mx-1">
             <Link to="/profile">Profile</Link>
           </li>
+
           <li className="mx-1">
             {/* this is not using the Link component to logout or user and then refresh the application to the start */}
             <a href="/" onClick={() => Auth.logout()}>
--- client/src/pages/LogIn.js
+++ client/src/pages/LogIn.js
@@ -1,40 +1,12 @@
 import React from 'react';
-import Comment from '../components/Profile/Comment';
-// import History from '../components/Profile/History';
-import Upload from '../components/Profile/Upload';
-import Skills from '../components/Profile/Skills';
-import {
-  Row, 
-  Col,
-  Container
-} from 'react-bootstrap';
-
 
 
 const Login =() => {
-const containerstyles={
-  width:"100%"
-}
-
 return(
     <div>
-
-    <Container fluid>
-      <Row>
-        <Col>
-          <Upload />
-        </Col>
-        <Col>
-          <Comment />
-        </Col>
-        <Col>
-          <Skills />
-        </Col>
-      </Row>
-    </Container>
-
+    <h1>THIS IS OUR LOGIN PAGE</h1>
   </div>
 )
 }
 
-export default Login;
+export default Login
