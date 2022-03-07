eb028eef6647d971e5bcac2bec041a350e669534
Edgar Fernandez <tempurakohi@gmail.com>
2022-03-07 10:59:54 -0800

    heroku file

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI ||'mongodb://localhost/profilesdb',
-  // 'mongodb+srv://kim:HVyLjD9d9272eyWCyB8uZat@webthebuilder.oihmy.mongodb.net/profilesdb',
-  { //replace uri with same uri from .env folder for proper connection
+mongoose.connect(process.env.MONGODB_URI ||'mongodb+srv://kim:HVyLjD9d9272eyWCyB8uZat@webthebuilder.oihmy.mongodb.net/profilesdb', { //replace uri with same uri from .env folder for proper connection
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
